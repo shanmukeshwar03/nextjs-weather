8be744a791d2c1595329d1572ba9fbe0746ebba7
shanmukeshwar03 <shanmukeshwar03@gmail.com>
2021-12-11 01:19:59 +0530

    minor updates

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   env: {
     BASE_URL: process.env.BASE_URL,
-    IP_STACK: process.env.IP_STACK,
-    TOKEN: process.env.TOKEN,
   },
 };
--- redux/weather.js
+++ redux/weather.js
@@ -1,33 +1,17 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const TOKEN = process.env.TOKEN;
-const BASE_URL = process.env.BASE_URL;
-
 export const fetchWeather = createAsyncThunk(
   "fetchWeather",
   async (payload) => {
-    if (payload) {
-      try {
-        const response = await axios.get(
-          `${BASE_URL}?q=${payload}&units=metric&appid=${TOKEN}`
-        );
-        return response.data;
-      } catch (error) {
-        const errors = error.response.data;
-        return { errors };
-      }
-    } else {
-      try {
-        const location = await axios.get(process.env.IP_STACK);
-        const response = await axios.get(
-          `${BASE_URL}?lat=${location.data.lat}&lon=${location.data.lon}&units=metric&appid=${TOKEN}`
-        );
-        return response.data;
-      } catch (error) {
-        const errors = error.response.data;
-        return { errors };
-      }
+    try {
+      const response = await axios.get(
+        `${process.env.BASE_URL}${payload ? "?city=" + payload : ""}`
+      );
+      return response.data;
+    } catch (error) {
+      const errors = error.response.data;
+      return { errors };
     }
   }
 );
