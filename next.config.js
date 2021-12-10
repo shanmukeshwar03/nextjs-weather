911feb20c8fc610a1cb2c46f8dde77cff0e962a2
shanmukeshwar03 <shanmukeshwar03@gmail.com>
2021-12-10 23:53:53 +0530

    minor updates

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  env: {
+    BASE_URL: process.env.BASE_URL,
+    IP_STACK: process.env.IP_STACK,
+    TOKEN: process.env.TOKEN,
+  },
+};
--- redux/weather.js
+++ redux/weather.js
@@ -1,52 +1,53 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-const BASE = 'https://api.openweathermap.org/data/2.5/weather'
-const TOKEN = '781481e32b959d093793524f27e2f93b'
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const TOKEN = process.env.TOKEN;
+const BASE_URL = process.env.BASE_URL;
 
 export const fetchWeather = createAsyncThunk(
-  'fetchWeather',
+  "fetchWeather",
   async (payload) => {
     if (payload) {
       try {
-        const res = await fetch(
-          `${BASE}?q=${payload}&units=metric&appid=${TOKEN}`
-        )
-        const wther = await res.json()
-        return wther
+        const response = await axios.get(
+          `${BASE_URL}?q=${payload}&units=metric&appid=${TOKEN}`
+        );
+        return response.data;
       } catch (error) {
-        console.log(error)
+        const errors = error.response.data;
+        return { errors };
       }
     } else {
       try {
-        const city = await fetch('https://ip.shanmukeshwar.me/')
-        const location = await city.json()
-        const res = await fetch(
-          `${BASE}?lat=${location.lat}&lon=${location.lon}&units=metric&appid=${TOKEN}`
-        )
-        const wther = await res.json()
-        return wther
+        const location = await axios.get(process.env.IP_STACK);
+        const response = await axios.get(
+          `${BASE_URL}?lat=${location.data.lat}&lon=${location.data.lon}&units=metric&appid=${TOKEN}`
+        );
+        return response.data;
       } catch (error) {
-        console.log(error)
+        const errors = error.response.data;
+        return { errors };
       }
     }
   }
-)
+);
 
 const weather = createSlice({
-  name: 'weather',
+  name: "weather",
   initialState: {
     modalOpen: false,
-    modalError: '',
+    modalError: "",
     weather: {
       coord: { lon: 0, lat: 0 },
       weather: [
         {
           id: 804,
-          main: 'Clouds',
-          description: 'overcast clouds',
-          icon: '04n',
+          main: "Clouds",
+          description: "overcast clouds",
+          icon: "04n",
         },
       ],
-      base: 'stations',
+      base: "stations",
       main: {
         temp: 0,
         feels_like: 0,
@@ -62,36 +63,37 @@ const weather = createSlice({
       sys: {
         type: 0,
         id: 0,
-        country: '',
+        country: "",
         sunrise: 0,
         sunset: 0,
       },
       timezone: 0,
       id: 0,
-      name: '',
+      name: "",
       cod: 0,
     },
   },
   reducers: {
     toggleModal: (state) => {
-      state.modalOpen = !state.modalOpen
+      state.modalOpen = !state.modalOpen;
     },
   },
   extraReducers: {
     [fetchWeather.pending]: (state, action) => {
-      state.modalError = ''
+      state.modalError = "";
     },
     [fetchWeather.fulfilled]: (state, action) => {
-      if (!action.payload.message) {
-        state.weather = action.payload
-        state.modalOpen = false
-        state.modalError = ''
-      } else {
-        state.modalError = action.payload.message || 'check typo'
+      if (!action.payload) return;
+      if (action.payload.errors) {
+        state.modalError = action.payload.message || "check typo";
+        return;
       }
+      state.weather = action.payload;
+      state.modalOpen = false;
+      state.modalError = "";
     },
   },
-})
+});
 
-export const { toggleModal } = weather.actions
-export default weather.reducer
+export const { toggleModal } = weather.actions;
+export default weather.reducer;
